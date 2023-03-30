11a0b65feb57e9151e1378259033e63e31b789ec
ramiro-olea <ror5687@hotmail.com>
2023-03-30 01:14:50 +0000

    Implement DynamoDB into our application

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,6 +3,7 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' ,'mock1@mock.com' ,'MOCK'),
   ('Andrew Bayko', 'bayko' ,'mock1@mock.com','MOCK');
+  ('Londo Mollari', 'londo' ,'lmollari@mock.com','MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
