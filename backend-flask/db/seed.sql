548bba82fe3cf596068f2274d13eec0e7b00b25c
ramiro-olea <ror5687@hotmail.com>
2023-03-30 03:09:51 +0000

    Debugging dynamodb issues

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'mock1@mock.com' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'mock1@mock.com','MOCK');
-  ('Londo Mollari', 'londo' ,'lmollari@mock.com','MOCK');
+  ('Ramiro Olea','ramolea','ror5687@hotmail.com' ,'97923303-18eb-4faa-b62b-e015405d194b'),
+  ('Andrew Bayko','bayko','bayko@exampro.co' ,'MOCK'),
+  ('Londo Mollari','londo','lmollari@centari.com' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'ramolea' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
